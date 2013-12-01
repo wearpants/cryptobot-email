e002fbd92bdd3713f1859710d615845a7fbd9345
Pete Fein <pete@wearpants.org>
2013-11-30 18:23:13 -0600

    initial checkin of build scripts

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload -s')
+    sys.exit()
+
+readme = open('README.rst').read()
+
+setup(
+    name='cryptobot-email',
+    version='0.1.0',
+    description='CryptoBot Email is an email bot that helps people learn how to use OpenPGP.',
+    long_description=readme,
+    author='EFF Tech Team',
+    author_email='tech@eff.org',
+    url='https://github.com/EFForg/cryptobot-email',
+    py_modules=[
+        'cryptobot_email',
+    ],
+    include_package_data=True,
+    install_requires=[
+        'Jinja2',
+    ],
+    license="AGPLv3+",
+    zip_safe=False,
+    keywords=['cryptobot-email', 'pgp', 'email'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Communications :: Email',
+    ],
+    test_suite='test',
+    entry_points = {"console_scripts": ["cryptobot_email = cryptobot_email:main"]},
+)
